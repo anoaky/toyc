07885be2e4f1629e017d3df3a196635da7a188cd
Anokhi S. <17suresa@gmail.com>
2025-11-10 13:25:41 -0500

    tweak run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
+while getopts "o" opt; do
+    case $opt in 
+        o)
+            showreport=1
+            ;;
+    esac
+done
 cargo run --example tests
 python tests/gen_report.py > tests/reports/report.html
-open tests/reports/report.html
+if [[ ! -z "$showreport" ]]; then
+    open tests/reports/report.html
+fi
